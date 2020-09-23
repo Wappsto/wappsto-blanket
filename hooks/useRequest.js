9ccb893e94a50340dc92e6b153542f22745c267b
Sami <dimkasami@gmail.com>
2020-09-23 13:06:15 +0200

    always remove old request cache

--- hooks/useRequest.js
+++ hooks/useRequest.js
@@ -1,9 +1,9 @@
-import { useMemo, useState, useCallback } from 'react';
+import { useMemo, useState, useCallback, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeRequestSelector } from 'wappsto-redux/selectors/request';
 import { makeRequest, removeRequest as removeStoreRequest } from 'wappsto-redux/actions/request';
 
-const useRequestSelector = (removeOldRequest) =>  {
+const useRequestSelector = (removeOldRequest=true) =>  {
   const dispatch = useDispatch();
   const [ requestId, setRequestId ] = useState();
   const getRequest = useMemo(makeRequestSelector, []);
@@ -14,13 +14,16 @@ const useRequestSelector = (removeOldRequest) =>  {
   }, [dispatch, requestId]);
 
   const send = useCallback((...args) => {
-    if(removeOldRequest && requestId){
-      removeRequest();
-    }
     const newId = dispatch(makeRequest(...args));
     setRequestId(newId);
     return newId;
   }, [dispatch]);
+
+  useEffect(() => () => {
+    if(removeOldRequest && requestId){
+      removeRequest();
+    }
+  }, [removeOldRequest, requestId, removeRequest]);
 
   return { request, requestId, setRequestId, send, removeRequest };
 }
