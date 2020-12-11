837c3007d8d8dac3760dcbad82181e5090b06c2c
mareklap <46565981+mareklap@users.noreply.github.com>
2020-12-11 14:46:14 +0100

    Add files via upload

---
+++ hooks/useMountedRef.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from 'react';
+
+export default () => {
+  const isMountedRef = useRef(true);
+
+  useEffect(() => {
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
+  return isMountedRef;
+}
