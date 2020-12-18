643e971bdb72a2a71d0dbe26c072919a11a46b1d
mareklap <46565981+mareklap@users.noreply.github.com>
2020-12-18 11:58:32 +0100

    Create useUser.js

---
+++ hooks/useUser.js
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import useRequest from 'wappsto-blanket/hooks/useRequest';
+import { getUserData } from 'wappsto-redux/selectors/entities';
+
+const useUser = () => {
+  const user = useSelector((state) => getUserData(state));
+  const { request, send } = useRequest();
+
+  useEffect(() => {
+    if(!user){
+      send({
+        method: 'get',
+        url: '/user',
+        query: { expand: 1 }
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let name = '';
+  if (user) {
+    if (user.nickname) {
+      name = user.nickname;
+    } else {
+      if (user.first_name) {
+        name += user.first_name + ' ';
+      }
+      if (user.last_name) {
+        name += user.last_name;
+      }
+
+      if (!name) {
+        if (user.provider[0] && user.provider[0].name) {
+          name = user.provider[0].name;
+        } else {
+          name = user.email;
+        }
+      }
+    }
+  }
+
+  const icon = user?.provider?.[0]?.picture;
+  const status = request?.status || 'pending';
+
+  return { status, icon, name };
+}
+
+export default useUser;
