603bb1874c51a6bd574f99e86afdde395a2d49cc
evelinamihailova <evelina@seluxit.com>
2021-04-23 11:22:07 +0200

    added roundBasedOnStep function

--- util/index.js
+++ util/index.js
@@ -88,3 +88,25 @@ export function cannotAccessState(state){
 export function getDottedText(text, num = 4, separator = ' ... '){
   return text.slice(0,num) + separator + text.slice(-num);
 }
+
+export function countDecimals(value) {
+  let text = value.toString()
+  // verify if number 0.000005 is represented as "5e-6"
+  if (text.indexOf('e-') > -1) {
+    let [base, trail] = text.split('e-');
+    let deg = parseInt(trail, 10);
+    return deg;
+  }
+  // count decimals for number in representation like "0.123456"
+  if (Math.floor(value) !== value) {
+    return value.toString().split(".")[1].length || 0;
+  }
+  return 0;
+}
+
+export function roundBasedOnStep(number, step, min) {
+  if(isNaN(number) || step === 0 ) { return number; }
+  let roundedNumber = step * Math.round(((number - min) / step)) + min;
+  roundedNumber = roundedNumber.toFixed(countDecimals(step));
+  return roundedNumber;
+}
