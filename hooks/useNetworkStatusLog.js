de5d5a61decfcaf8fb0bf50001e93839b5a3a6d3
mareklap <46565981+mareklap@users.noreply.github.com>
2021-05-03 19:52:30 +0200

    reorganized useNetworkStatusLog hook

--- hooks/useNetworkStatusLog.js
+++ hooks/useNetworkStatusLog.js
@@ -11,37 +11,7 @@ let cache = {};
 
 onLogout(() => cache = {});
 
-const getData = async ({ networkId, query, isMountedRef, initLimit, store }) => {
-  let data = [];
-  let more = true;
-
-  while(more && isMountedRef.current) {
-    const state = store.getState();
-    const sessionObj = getSession(state);
-    const url = `/log/${networkId}/online_iot?${querystring.stringify(query)}`;
-    const http = await startRequest(store.dispatch, { url, method: 'GET' }, sessionObj);
-    if(!isMountedRef.current) {
-      return;
-    }
-    if(http.status !== 200) {
-      throw http.json;
-    }
-    data.push(...http.json.data);
-    if (data.length < initLimit) {
-      more = http.json.more;
-      if(more) {
-        const last = data.pop();
-        query.start = last.time;
-      }
-    } else {
-      more = false;
-    }
-  }
-
-  return data.reverse();
-}
-
-const useNetworkStatusLog = (initNetworkId) => {
+const useNetworkStatusLog = (networkId) => {
   const [{ data, status }, setResult] = useState({});
   const functionRef = useRef({});
   const isMountedRef = useRef(true);
@@ -52,8 +22,39 @@ const useNetworkStatusLog = (initNetworkId) => {
     return () => isMountedRef.current = false;
   }, []);
 
-  const _get = ({ start, end, networkId=initNetworkId, limit: initLimit, resetCache }) => {
+  const _get = ({ start, end, limit: initLimit, resetCache }) => {
+    const getData = async ({ query, initLimit }) => {
+      let data = [];
+      let more = true;
+    
+      while(more && isMountedRef.current) {
+        const state = store.getState();
+        const sessionObj = getSession(state);
+        const url = `/log/${networkId}/online_iot?${querystring.stringify(query)}`;
+        const http = await startRequest(store.dispatch, { url, method: 'GET' }, sessionObj);
+        if(!isMountedRef.current) {
+          return [];
+        }
+        if(http.status !== 200) {
+          throw http.json;
+        }
+        data.push(...http.json.data);
+        if (data.length < initLimit) {
+          more = http.json.more;
+          if(more) {
+            const last = data.pop();
+            query.start = last.time;
+          }
+        } else {
+          more = false;
+        }
+      }
+    
+      return data;
+    }
+
     currentRef.current = {};
+
     if(!networkId) {
       return;
     }
@@ -78,14 +79,16 @@ const useNetworkStatusLog = (initNetworkId) => {
 
     const current = currentRef.current;
     const query = { limit, order: 'descending' };
-    if (start && end) {
+    if (start) {
       query.start = start;
+    }
+    if (end) {
       query.end = end;
     }
 
-    setResult({ status: STATUS.pending, data });
+    setResult((current) => ({ ...current, status: STATUS.pending }));
 
-    getData({ networkId, query, isMountedRef, initLimit, store }).then((data) => {
+    getData({ query, initLimit }).then((data) => {
       if(isMountedRef.current && currentRef.current === current) {
         cache[networkId] = data;
         setResult({ status: STATUS.success, data });
