fe1fd9956ccdbdfa500196bf5534a22a604969ab
Andreas Bomholtz <andreas@seluxit.com>
2022-03-23 15:32:40 +0100

    Fixed issue with usePrevious test

--- __tests__/usePrevious.test.js
+++ __tests__/usePrevious.test.js
@@ -1,11 +1,19 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import { usePrevious } from '../src';
 
 describe('usePrevious', () => {
-  it('runs correctly', () => {
-    const { result } = renderHook(() => {
-      usePrevious({ key: 'test' });
+  it('runs correctly', async () => {
+    const value = 'test';
+    const { result, rerender } = renderHook(({ value }) => usePrevious(value), {
+      initialProps: { value }
     });
+
     expect(result.current).toEqual(undefined);
+
+    await act(async () => {
+      await rerender();
+    });
+
+    expect(result.current).toEqual('test');
   });
 });
