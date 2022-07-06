1e519b3b96d2ca4722624eac5bca69da83e33a6e
Andreas Bomholtz <andreas@seluxit.com>
2022-07-06 10:59:11 +0200

    Fixed error in useMounted

--- src/hooks/useMounted.js
+++ src/hooks/useMounted.js
@@ -3,12 +3,14 @@ import { useEffect, useRef } from 'react';
 export default function useMounted() {
   const isMountedRef = useRef(true);
 
-  useEffect(
-    () => () => {
+  useEffect(() => {
+    if (!isMountedRef.current) {
+      isMountedRef.current = true;
+    }
+    return () => {
       isMountedRef.current = false;
-    },
-    [],
-  );
+    };
+  }, []);
 
   return isMountedRef;
 }
