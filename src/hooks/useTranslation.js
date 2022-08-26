8815f4b604fe3b0f38dd7f101d26efa5d71c9bc8
Andreas Bomholtz <andreas@seluxit.com>
2022-08-26 11:31:05 +0200

    Prettify

--- src/hooks/useTranslation.js
+++ src/hooks/useTranslation.js
@@ -2,22 +2,31 @@ import { useCallback } from 'react';
 
 const useTranslation = (t) => {
   // translate + Capitalize
-  const tC = useCallback((str, options) => {
-    const newStr = str ? t(str, options) : '';
-    return newStr.charAt(0).toUpperCase() + newStr.slice(1);
-  }, [t]);
+  const tC = useCallback(
+    (str, options) => {
+      const newStr = str ? t(str, options) : '';
+      return newStr.charAt(0).toUpperCase() + newStr.slice(1);
+    },
+    [t],
+  );
 
   // translate + Capitalize Each
-  const tCE = useCallback((str, options) => {
-    const newStr = str ? t(str, options) : '';
-    return newStr.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1));
-  }, [t]);
+  const tCE = useCallback(
+    (str, options) => {
+      const newStr = str ? t(str, options) : '';
+      return newStr.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1));
+    },
+    [t],
+  );
 
   // translate + Uppercase
-  const tU = useCallback((str, options) => {
-    const newStr = str ? t(str, options) : '';
-    return newStr.toUpperCase();
-  }, [t]);
+  const tU = useCallback(
+    (str, options) => {
+      const newStr = str ? t(str, options) : '';
+      return newStr.toUpperCase();
+    },
+    [t],
+  );
 
   return { t, tC, tCE, tU };
 };
